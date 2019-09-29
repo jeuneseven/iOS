2e9ba9e7d59cbde07033d8d810ef3a5752a3fbdf
jeuneseven <jeuneseven@gmail.com>
2019-09-29 14:56:07 +0800

    block循环引用 init

---
+++ BlockRetainCycle/BlockRetainCycle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlockRetainCycle
+//
+//  Created by kkk on 2019/9/29.
+//  Copyright © 2019 kkk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
