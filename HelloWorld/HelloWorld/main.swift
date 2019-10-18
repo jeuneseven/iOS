9a06d06267f47ff400c31c6a24e424e88c6a3345
kkk <jeuneseven@gmail.com>
2019-10-18 08:46:11 +0800

    swift init；playground init

---
+++ HelloWorld/HelloWorld/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by kkk on 2019/10/18.
+//  Copyright © 2019 kkk. All rights reserved.
+//
+// 不用编写main函数，swift默认以执行语句的第一句作为程序入口
+import Foundation
+//打印语句自动换行
+print("Hello, World!")
+
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+var a = 10
+var b = 20
+print (a + b)
+let c = a + b
+//插入格式化符号使用\加()即可
+print("hello world \(c)")
+
+import UIKit
+import PlaygroundSupport
+
+let view = UIView()
+view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+view.backgroundColor = UIColor.blue
+PlaygroundPage.current.liveView = view
+
