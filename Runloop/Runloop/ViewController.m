29ac98aa6c1de67b2ae7af7a711d39fcb3389c0a
jeuneseven <jeuneseven@gmail.com>
2019-10-23 13:15:10 +0800

    runloop包含内容

--- Runloop/Runloop/ViewController.m
+++ Runloop/Runloop/ViewController.m
@@ -28,7 +28,22 @@
      线程刚创建时没有runloop对象，runloop会在第一次获取它时创建
      runloop会在线程结束时销毁
      主线程的runloop已经自动获取（创建），子线程默认没有开启runloop
+     runloop中有多个mode，currentMode 表示当前的mode，需要切换时，要退出当前mode，重新选择mode进入，进入后如果发现没有包含任何内容的话，就会退出
+     mode中包含：source0，source1，observer，timer
+     
+     runloop循环处理以下内容：
+     source0:触摸事件处理、performSelector:onThread:
+     source1:基于port的线程间通信、系统事件的捕捉（捕捉后分发给source0处理）
+     timer:timer、performSelector:onThread:afterDelay:
+     observer:监听runloop状态、UI刷新、autoreleasepool
      */
+    
+    //常见模式：
+    kCFRunLoopDefaultMode;
+    NSDefaultRunLoopMode;
+    //kCFRunLoopCommonModes包括kCFRunLoopDefaultMode、
+//    CFRunLoopObserverCreate(<#CFAllocatorRef allocator#>, <#CFOptionFlags activities#>, <#Boolean repeats#>, <#CFIndex order#>, <#CFRunLoopObserverCallBack callout#>, NULL);
+//    CFRunLoopAddObserver(<#CFRunLoopRef rl#>, <#CFRunLoopObserverRef observer#>, kCFRunLoopCommonModes)
 }
 
 
