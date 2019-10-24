a8eddef0ba7799ed76538177e5bc47414a2507d9
jeuneseven <jeuneseven@gmail.com>
2019-10-24 16:15:05 +0800

    监听runloop状态

--- Runloop/Runloop/ViewController.m
+++ Runloop/Runloop/ViewController.m
@@ -31,6 +31,8 @@
      runloop中有多个mode，currentMode 表示当前的mode，需要切换时，要退出当前mode，重新选择mode进入，进入后如果发现没有包含任何内容的话，就会退出
      mode中包含：source0，source1，observer，timer
      
+     runloop休眠与线程阻塞不同，是从用户态——>内核态的状态转换mach_msg()
+     
      runloop循环处理以下内容：
      source0:触摸事件处理、performSelector:onThread:
      source1:基于port的线程间通信、系统事件的捕捉（捕捉后分发给source0处理）
@@ -42,9 +44,44 @@
     kCFRunLoopDefaultMode;
     NSDefaultRunLoopMode;
     //kCFRunLoopCommonModes包括kCFRunLoopDefaultMode、
-//    CFRunLoopObserverCreate(<#CFAllocatorRef allocator#>, <#CFOptionFlags activities#>, <#Boolean repeats#>, <#CFIndex order#>, <#CFRunLoopObserverCallBack callout#>, NULL);
-//    CFRunLoopAddObserver(<#CFRunLoopRef rl#>, <#CFRunLoopObserverRef observer#>, kCFRunLoopCommonModes)
+    CFRunLoopObserverRef observer = CFRunLoopObserverCreateWithHandler(kCFAllocatorDefault, kCFRunLoopAllActivities, YES, 0, ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
+        switch (activity) {
+            case kCFRunLoopEntry: {
+                NSLog(@"kCFRunLoopEntry");
+                break;
+            }
+            case kCFRunLoopBeforeTimers: {
+                NSLog(@"kCFRunLoopBeforeTimers");
+                break;
+            }
+            case kCFRunLoopBeforeSources: {
+                NSLog(@"kCFRunLoopBeforeSources");
+                break;
+            }
+            case kCFRunLoopBeforeWaiting: {
+                NSLog(@"kCFRunLoopBeforeWaiting");
+                break;
+            }
+            case kCFRunLoopAfterWaiting: {
+                NSLog(@"kCFRunLoopAfterWaiting");
+                break;
+            }
+            case kCFRunLoopExit: {
+                NSLog(@"kCFRunLoopExit");
+                break;
+            }
+            default:
+                break;
+        }
+    });
+    //添加监听到runloop中
+    CFRunLoopAddObserver(CFRunLoopGetMain(), observer, kCFRunLoopCommonModes);
+    CFRelease(observer);
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        dispatch_async(dispatch_get_main_queue(), ^{//只有切换到主线程队列执行时才会涉及到runloop，其他GCD不涉及runloop
+            ;
+        });
+    });
 }
-
 
 @end
