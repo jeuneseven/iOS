8d629a4d31fbbe64e2568b53f960b8f5cc8292f0
jeuneseven <jeuneseven@gmail.com>
2020-05-17 21:44:46 +0800

    ApplicationCycle init

---
+++ ApplicationCycle/ApplicationCycle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ApplicationCycle
+//
+//  Created by 李占昆 on 2020/5/17.
+//  Copyright © 2020 kkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
