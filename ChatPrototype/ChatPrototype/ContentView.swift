47b903909981a65ee1029b7247c7dc9e80fbe3a9
jeuneseven <jeuneseven@gmail.com>
2024-04-26 20:45:34 +1200

    https://developer.apple.com/tutorials/develop-in-swift/customize-views-with-properties#Create-a-custom-subview

--- ChatPrototype/ChatPrototype/ContentView.swift
+++ ChatPrototype/ChatPrototype/ContentView.swift
@@ -9,15 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Knock, knock!")
-                .padding()
-                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
-            Text("Who's there?")
-                .padding()
-                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+        HStack {
+            VStack {
+                Text("Monday")
+                Image(systemName: "sun.max.fill")
+                    .foregroundColor(Color.yellow)
+                Text("High: 30")
+                Text("Low: 15")
+            }.padding()
+            VStack {
+                Text("Tuesday")
+                Image(systemName: "cloud.rain.fill")
+                    .foregroundColor(Color.blue)
+                Text("High: 20")
+                Text("Low: 11")
+            }.padding()
         }
-        .padding()
+//        VStack {
+//            Text("Knock, knock!")
+//                .padding()
+//                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+//                .shadow(radius: 4)
+//            Text("Who's there?")
+//                .padding()
+//                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+//        }
+//        .padding()
+//        HStack {
+//            Text("Knock, knock!")
+//                .padding()
+//                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+//                .shadow(radius: 4)
+//            Text("Who's there?")
+//                .padding()
+//                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+//        }
     }
 }
 
