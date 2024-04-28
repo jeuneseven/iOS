3dc9b36af87736cae3508e0d14cba06bf6633b47
jeuneseven <jeuneseven@gmail.com>
2024-04-28 21:21:50 +1200

    https://developer.apple.com/tutorials/develop-in-swift/design-an-interface

---
+++ OnboardingFlow/OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by seven on 2024/4/28.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
