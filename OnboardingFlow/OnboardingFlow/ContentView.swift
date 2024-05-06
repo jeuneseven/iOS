9e48174c918f5903bdcf0681f48a67da76703747
jeuneseven <jeuneseven@gmail.com>
2024-05-06 17:27:45 +1200

    https://developer.apple.com/tutorials/develop-in-swift/design-an-interface#Create-a-theme-color-and-polish-the-style

--- OnboardingFlow/OnboardingFlow/ContentView.swift
+++ OnboardingFlow/OnboardingFlow/ContentView.swift
@@ -7,13 +7,20 @@
 
 import SwiftUI
 
+let gradientColors: [Color] = [
+    .pink,
+    .red
+]
+
 struct ContentView: View {
     var body: some View {
         TabView {
             WelcomePage()
             FeaturesPage()
         }
-        .tabViewStyle(.page)
+
+//        .background(Gradient(colors: gradientColors))
+                .tabViewStyle(.page)
     }
 }
 
