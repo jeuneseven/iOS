1b8b1cd063792513cb6e0723e488acfc79a89662
jeuneseven <jeuneseven@gmail.com>
2024-05-11 19:01:20 +1200

    https://developer.apple.com/tutorials/develop-in-swift/update-the-ui-with-state

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by seven on 2024/5/11.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiceRoller/DiceRoller/DiceView.swift
@@ -0,0 +1,20 @@
+//
+//  DiceView.swift
+//  DiceRoller
+//
+//  Created by seven on 2024/5/11.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DiceView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiceView()
+    }
+}
