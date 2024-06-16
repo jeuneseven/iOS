be7b863f97bc6420838b45db9de6a76038931bd9
jeuneseven <jeuneseven@gmail.com>
2024-06-16 19:56:30 +1200

    TodoList Swift init

---
+++ Swift/TodoList/TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by seven on 2024/6/16.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
