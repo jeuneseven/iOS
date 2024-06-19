0ff922d91f62c222356753bee1ef3b2dc0591cfe
jeuneseven <jeuneseven@gmail.com>
2024-06-19 21:21:02 +1200

    Update ContentView.swift

--- Swift/TodoList/TodoList/ContentView.swift
+++ Swift/TodoList/TodoList/ContentView.swift
@@ -7,15 +7,57 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Text("TodoList")
+struct TableViewWrapper: UIViewControllerRepresentable {
+    class Coordinator: NSObject, UITableViewDataSource, UITableViewDelegate {
+        var parent: TableViewWrapper
+
+        init(parent: TableViewWrapper) {
+            self.parent = parent
         }
-        .padding()
+
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return parent.items.count
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            cell.textLabel?.text = parent.items[indexPath.row]
+            return cell
+        }
+    }
+
+    var items: [String]
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+
+    func makeUIViewController(context: Context) -> UITableViewController {
+        let tableViewController = UITableViewController()
+        tableViewController.tableView.dataSource = context.coordinator
+        tableViewController.tableView.delegate = context.coordinator
+        tableViewController.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        return tableViewController
+    }
+
+    func updateUIViewController(_ uiViewController: UITableViewController, context: Context) {
+        uiViewController.tableView.reloadData()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View {
+    var body: some View {
+        TableViewWrapper(items: ["Item 1", "Item 2", "Item 3", "Item 4"])
+            .navigationBarTitle("UITableView Example", displayMode: .inline)
+    }
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+//#Preview {
+//    ContentView()
+//}
