be2909a8dc1fa309cba04dce2ed59356dbe55d72
jeuneseven <jeuneseven@gmail.com>
2024-06-23 21:21:09 +1200

    Update Contents.swift

--- Swift/MyPlayground.playground/Pages/Page02.xcplaygroundpage/Contents.swift
+++ Swift/MyPlayground.playground/Pages/Page02.xcplaygroundpage/Contents.swift
@@ -2,10 +2,17 @@
 
 import Foundation
 
+var dictionary = [123: 123]
+print(dictionary)
+
+var set = ([123, 234])
+print(set)
+
 var array:[Int] = [123, 123, 123]
 print(array)
 
-var tupple = (1, 1, 2, 2, 3)
+var tupple = (1, 2)
+tupple.0 = 2
 print(tupple)
 
 
