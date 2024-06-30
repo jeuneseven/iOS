9b46d198d34163dad05f716952afb313a6000ab7
jeuneseven <jeuneseven@gmail.com>
2024-06-30 21:13:55 +1200

    Update Contents.swift

--- Swift/MyPlayground.playground/Pages/Page03.xcplaygroundpage/Contents.swift
+++ Swift/MyPlayground.playground/Pages/Page03.xcplaygroundpage/Contents.swift
@@ -5,3 +5,29 @@ var cleanRoom = { (name: String, by: String) in
 }
 cleanRoom("", "")
 
+// Trailing closure syntax
+
+func holdClass(name: String, lesson: () -> Void) {
+    print("Welcome to \(name)!")
+    lesson()
+    print("Make sure your homework is done by next week.")
+}
+holdClass(name: "Philosophy 101", lesson: {
+    print("All we are is dust in the wind, dude.")
+})
+//holdClass("Philosophy 101", lesson:) {
+//    print("All we are is dust in the wind, dude.")
+//}
+
+func tendGarden(activities: () -> Void) {
+    print("I love gardening")
+    activities()
+}
+
+tendGarden(activities: {
+    print("Let's grow some roses!")
+})
+
+tendGarden {
+    print("Let's grow some roses!")
+}
