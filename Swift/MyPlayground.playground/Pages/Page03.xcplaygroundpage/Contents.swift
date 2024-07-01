0868ff6744ceea620d5d27df5a2c126018173ff7
jeuneseven <jeuneseven@gmail.com>
2024-07-01 18:20:06 +1200

    Update Contents.swift

--- Swift/MyPlayground.playground/Pages/Page03.xcplaygroundpage/Contents.swift
+++ Swift/MyPlayground.playground/Pages/Page03.xcplaygroundpage/Contents.swift
@@ -31,3 +31,77 @@ tendGarden(activities: {
 tendGarden {
     print("Let's grow some roses!")
 }
+
+func getMeasurement(handler: (Double) -> Void) {
+    let measurement = 32.2
+    handler(measurement)
+}
+getMeasurement { (measurement: Double) in
+    print("It measures \(measurement).")
+}
+
+func processPrimes(using closure: (Int) -> Void) {
+    let primes = [2, 3, 5, 7, 11, 13, 17, 19]
+    for prime in primes {
+        closure(prime)
+    }
+}
+processPrimes { (prime: Int) in
+    print("\(prime) is a prime number.")
+    let square = prime * prime
+    print("\(prime) squared is \(square)")
+}
+
+func manipulate(numbers: [Int], using algorithm: (Int) -> Int) {
+    for number in numbers {
+        let result = algorithm(number)
+        print("Manipulating \(number) produced \(result)")
+    }
+}
+manipulate(numbers: [1, 2, 3]) { number in
+    return number * number
+}
+
+func scoreToGrade(score: Int, gradeMapping: (Int) -> String) {
+    print("Your score was \(score)%.")
+    let result = gradeMapping(score)
+    print("That's a \(result).")
+}
+scoreToGrade(score: 80) { (grade: Int) in
+    if grade < 85 {
+        return "Fail"
+    }
+    
+    return "0"
+}
+
+func goShopping(for item: String, decisionHandler: (String) -> Bool) {
+    print("I'm going to buy \(item)")
+    let returnValue = decisionHandler(item)
+    if returnValue == true {
+        print("Great! I bought them.")
+    } else {
+        print("Maybe next time...")
+    }
+}
+
+goShopping(for: "shoes") { item in
+    if item == "shoes" {
+        return false
+    } else {
+        return true
+    }
+}
+
+
+func createValidator() -> (String) -> Bool {
+    return {
+        if $0 == "twostraws" {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+let validator = createValidator()
+print(validator("twostraws"))
