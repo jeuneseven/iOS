c938cd89d037693eff2648570c44e72386778e57
jeuneseven <jeuneseven@gmail.com>
2024-07-03 22:00:47 +1200

    Update Contents.swift

--- Swift/MyPlayground.playground/Pages/Page03.xcplaygroundpage/Contents.swift
+++ Swift/MyPlayground.playground/Pages/Page03.xcplaygroundpage/Contents.swift
@@ -121,3 +121,18 @@ if language == "French" {
 }
 let greeting = makeGreeting(language: "English")
 greeting("Paul")
+
+//Capturing values
+func storeTwoValues(value1: String, value2: String) -> (String) -> String {
+    var previous = value1
+    var current = value2
+    return { new in
+        let removed = previous
+        previous = current
+        current = new
+        return "Removed \(removed)"
+    }
+}
+let store = storeTwoValues(value1: "Hello", value2: "World")
+let removed = store("Value Three")
+print(removed)
