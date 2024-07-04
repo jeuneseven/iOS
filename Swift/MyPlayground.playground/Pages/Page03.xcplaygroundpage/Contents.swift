1f883e2a5126ebfebf80faacf4f4c5c6876c717d
jeuneseven <jeuneseven@gmail.com>
2024-07-04 22:32:48 +1200

    Update Contents.swift

--- Swift/MyPlayground.playground/Pages/Page03.xcplaygroundpage/Contents.swift
+++ Swift/MyPlayground.playground/Pages/Page03.xcplaygroundpage/Contents.swift
@@ -136,3 +136,16 @@ func storeTwoValues(value1: String, value2: String) -> (String) -> String {
 let store = storeTwoValues(value1: "Hello", value2: "World")
 let removed = store("Value Three")
 print(removed)
+
+func visitPlaces() -> (String) -> Void {
+    var places = [String: Int]()
+    return {
+        places[$0, default: 0] += 1
+        let timesVisited = places[$0, default: 0]
+        print("Number of times I've visited \($0): \(timesVisited).")
+    }
+}
+let visit = visitPlaces()
+visit("London")
+visit("New York")
+visit("London")
