5d88e2ef2e9a8bfdb37ff8c072b7000e6ce7e2d6
jeuneseven <jeuneseven@gmail.com>
2024-07-09 19:06:36 +1200

    Update Contents.swift

--- Swift/MyPlayground.playground/Pages/Page05.xcplaygroundpage/Contents.swift
+++ Swift/MyPlayground.playground/Pages/Page05.xcplaygroundpage/Contents.swift
@@ -1,1 +1,46 @@
-// Class
+// Class inheritance
+class Vehicle {
+    var wheels: Int
+    init(wheels: Int) {
+        self.wheels = wheels
+    }
+}
+class Truck: Vehicle {
+    var goodsCapacity: Int
+    init(wheels: Int, goodsCapacity: Int) {
+        self.goodsCapacity = goodsCapacity
+        super.init(wheels: wheels)
+    }
+}
+
+//Copying objects
+struct GalacticaCrew {
+    var isCylon = false
+}
+var starbuck = GalacticaCrew()
+var tyrol = starbuck
+tyrol.isCylon = true
+print(starbuck.isCylon)
+print(tyrol.isCylon)
+
+class Starship {
+    var maxWarp = 9.0
+}
+var voyager = Starship()
+voyager.maxWarp = 9.975
+var enterprise = voyager
+enterprise.maxWarp = 9.6
+print(voyager.maxWarp)
+print(enterprise.maxWarp)
+
+struct Calculator {
+    var currentTotal = 0
+}
+var baseModel = Calculator()
+var casio = baseModel
+var texas = baseModel
+casio.currentTotal = 556
+texas.currentTotal = 384
+print(casio.currentTotal)
+print(texas.currentTotal)
+print(baseModel.currentTotal)
