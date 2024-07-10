29777e8c4d496b74a9e08cbd646c401f892599d4
jeuneseven <jeuneseven@gmail.com>
2024-07-11 00:00:24 +1200

    Update Contents.swift

--- Swift/MyPlayground.playground/Pages/Page06.xcplaygroundpage/Contents.swift
+++ Swift/MyPlayground.playground/Pages/Page06.xcplaygroundpage/Contents.swift
@@ -1,5 +1,11 @@
 //Protocols
 
+protocol DogTrainer {
+    func train(dog: String) {
+        print("We'll make \(dog) behave in no time!")
+    }
+}
+
 //needs a type annotation
 protocol Swimmable {
     var depth: Double { get set }
