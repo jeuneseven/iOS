adfa799e73ed3b3d06f29a6073a965b96f1ca002
jeuneseven <jeuneseven@gmail.com>
2024-07-13 20:52:03 +1200

    Update Contents.swift

--- Swift/MyPlayground.playground/Pages/Page07.xcplaygroundpage/Contents.swift
+++ Swift/MyPlayground.playground/Pages/Page07.xcplaygroundpage/Contents.swift
@@ -1,5 +1,10 @@
 // Optionals
 
+let tableHeight: Double? = 100
+if tableHeight! > 85.0 {
+    print("The table is too high.")
+}
+
 let album = "Red"
 let albums = ["Reputation", "Red", "1989"]
 if let position = albums.firstIndex(of: album) {
