e1eaaa8cffcdb3cf5f01de8adb2a3e396f32b7e6
jeuneseven <jeuneseven@gmail.com>
2024-07-14 10:52:37 +1200

    Update Contents.swift

--- Swift/MyPlayground.playground/Pages/Page07.xcplaygroundpage/Contents.swift
+++ Swift/MyPlayground.playground/Pages/Page07.xcplaygroundpage/Contents.swift
@@ -11,3 +11,47 @@ if let position = albums.firstIndex(of: album) {
     print("Found \(album) at position \(position).")
 }
 
+//Unwrapping with guard
+func double(number: Int?) -> Int? {
+    guard let number = number else {
+        return nil
+    }
+    return number * 2
+}
+let input = 5
+if let doubled = double(number: input) {
+    print("\(input) doubled is \(doubled).")
+}
+
+func add3(to number: Int?) -> Int {
+    guard let number = number else {
+        return 3
+    }
+    return number + 3
+}
+let added = add3(to: 5)
+print(added)
+
+func username(for id: Int?) -> String? {
+    guard let id = id else {
+        return nil
+    }
+    if id == 1989 {
+        return "Taylor Swift"
+    } else {
+        return nil
+    }
+}
+if let user = username(for: 1989) {
+    print("Hello, \(user)!")
+}
+
+//Nil coalescing
+let lightsaberColor: String? = "green"
+let color = lightsaberColor ?? "blue"
+
+var conferenceName: String? = "WWDC"
+var conference: String? = conferenceName ?? nil
+
+let distanceRan: Double? = 0.5
+let distance: Double = distanceRan ?? 0
