68ce28f314382e3e32d6c9dde6ce58744b87f51a
jeuneseven <jeuneseven@gmail.com>
2024-07-18 21:44:39 +1200

    WeSplit init

---
+++ Swift/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by seven on 2024/7/18.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
