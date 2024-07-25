7c16a35c8febe1f70878a80b09857b5d9c797c72
jeuneseven <jeuneseven@gmail.com>
2024-07-25 22:17:56 +1200

    Update ContentView.swift

--- Swift/WeSplit/WeSplit/ContentView.swift
+++ Swift/WeSplit/WeSplit/ContentView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct ContentView: View {
     @State var tapCount = 0
     @State private var name = ""
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
 
     var body: some View {
         NavigationStack {
