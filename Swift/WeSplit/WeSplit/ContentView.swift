511e9f131bf5e410e7cc55dcca905b69836db2cd
jeuneseven <jeuneseven@gmail.com>
2024-08-01 20:34:51 +1200

    Update ContentView.swift

--- Swift/WeSplit/WeSplit/ContentView.swift
+++ Swift/WeSplit/WeSplit/ContentView.swift
@@ -8,51 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tapCount = 0
-    @State private var name = ""
-    
-    @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2
-    @State private var tipPercentage = 20
-
     var body: some View {
-        NavigationStack {
-            Form {
-                Section {
-                    TextField("Enter your name", text: $name)
-                }
-            }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
-            Form {
-                Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                        .keyboardType(.decimalPad)
-
-                }
-                Section {
-                        Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    }
-                Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                        .keyboardType(.decimalPad)
-
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2..<100) {
-                            Text("\($0) people")
-                        }
-                    }
-                }
-            }
-            Form {
-                ForEach(0..<10) {
-                    Text("Row \($0)")
-                }
-            }
-        }
-        
-        Button("Tap Count: \(tapCount)") {
-            self.tapCount += 1
+        VStack {
+            
         }
     }
 }
