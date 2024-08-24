c4289378c42e0d7d3ca91e1e61924b3da1831d34
jeuneseven <jeuneseven@gmail.com>
2024-08-24 22:07:36 +1200

    Update ContentView.swift

--- Swift/WeSplit/WeSplit/ContentView.swift
+++ Swift/WeSplit/WeSplit/ContentView.swift
@@ -8,56 +8,56 @@
 import SwiftUI
 //conform View protocol, must return some view
 struct ContentView: View {
-//    @State private var tapCount = 0
-//    @State private var name = ""
-//    
-//    let students = ["Harry", "Hermione", "Ron"]
-//        @State private var selectedStudent = "Harry"
+    @State private var tapCount = 0
+    @State private var name = ""
     
-//    @State private var checkAmount = 0.0
-//    @State private var numberOfPeople = 2
-//    @State private var tipPercentage = 20
+    let students = ["Harry", "Hermione", "Ron"]
+        @State private var selectedStudent = "Harry"
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
     
     var body: some View {
-//        Form {
-//            Section {
-//                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-//            }
-//
-//            Section {
-//                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-//                    .keyboardType(.decimalPad)
-//            }
-//        }
-//        NavigationStack {
-//            Form {
-//                Section {
-//                    TextField("Enter your name: ", text: self.$name)
-//                    Text("Your name is \(name)")
-//                }
-//                
-//                Section {
-//                    ForEach(0..<10) {
-//                        Text("row \($0)")
-//                    }
-//                }
-//                
-//                Section {
-//                    Picker("Select your student", selection: $selectedStudent) {
-//                                        //the strings themselves are unique.
-//                                        ForEach(students, id: \.self) {
-//                                            Text($0)
-//                                        }
-//                                    }
-//                }
-//            }
-//            .navigationTitle("Title")
-//            .navigationBarTitleDisplayMode(.inline)
-//        }
-//        
-//        Button("tapCount : \(tapCount)") {
-//            self.tapCount += 1
-//        }
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
+
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+        }
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Enter your name: ", text: self.$name)
+                    Text("Your name is \(name)")
+                }
+                
+                Section {
+                    ForEach(0..<10) {
+                        Text("row \($0)")
+                    }
+                }
+                
+                Section {
+                    Picker("Select your student", selection: $selectedStudent) {
+                                        //the strings themselves are unique.
+                                        ForEach(students, id: \.self) {
+                                            Text($0)
+                                        }
+                                    }
+                }
+            }
+            .navigationTitle("Title")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        
+        Button("tapCount : \(tapCount)") {
+            self.tapCount += 1
+        }
     }
 }
 
