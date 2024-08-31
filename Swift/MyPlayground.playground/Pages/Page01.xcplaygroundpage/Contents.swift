9350762ec72ae4363cdb106fc95bf2e1f3e340d3
jeuneseven <jeuneseven@gmail.com>
2024-08-31 19:45:36 +1200

    Update Contents.swift

--- Swift/MyPlayground.playground/Pages/Page01.xcplaygroundpage/Contents.swift
+++ Swift/MyPlayground.playground/Pages/Page01.xcplaygroundpage/Contents.swift
@@ -49,13 +49,3 @@ repeat {
     bool = false
 } while bool
 
-
-import UIKit
-import PlaygroundSupport
-
-let view = UIView()
-view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-view.backgroundColor = UIColor.blue
-PlaygroundPage.current.liveView = view
-
-
