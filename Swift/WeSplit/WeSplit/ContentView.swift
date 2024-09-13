5ef12892fbfb1236adcc7fdc3f72848e1fac2960
jeuneseven <jeuneseven@gmail.com>
2024-09-13 20:53:06 +1200

    Update ContentView.swift

--- Swift/WeSplit/WeSplit/ContentView.swift
+++ Swift/WeSplit/WeSplit/ContentView.swift
@@ -24,6 +24,12 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .keyboardType(.decimalPad)
+
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
                 }
                 Section {
                     TextField("Enter your name: ", text: self.$name)
