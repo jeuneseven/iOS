e01f953946992bd10ee9e4932480f7f9d38b64f5
jeuneseven <jeuneseven@gmail.com>
2024-10-13 08:42:50 +1300

    ViewsAndModifiers init

---
+++ Swift/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by seven on 2024/10/13.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
