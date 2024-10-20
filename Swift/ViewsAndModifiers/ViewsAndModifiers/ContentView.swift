1e6c0a40428c5720cacc667231e578687f275e81
jeuneseven <jeuneseven@gmail.com>
2024-10-20 14:02:32 +1300

    Update ContentView.swift

--- Swift/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Swift/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -25,6 +25,17 @@ struct CapsuleText: View {
     }
 }
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
 struct ContentView: View {
     @State private var useRedText = false
     
