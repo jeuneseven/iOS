92a27b74d60ea84ee794cf1f22832f53c62fbd20
jeuneseven <jeuneseven@gmail.com>
2024-10-25 09:42:21 +1300

    Update ContentView.swift

--- Swift/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Swift/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,35 +7,11 @@
 
 import SwiftUI
 
-struct Watermark: ViewModifier {
-    var text: String
-
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundStyle(.white)
-                .padding(5)
-                .background(.black)
-        }
-    }
-}
-
-extension View {
-    func watermarked(with text: String) -> some View {
-        modifier(Watermark(text: text))
-    }
-}
-
 struct ContentView: View {
     @State private var useRedText = false
     
     var body: some View {
-        VStack(spacing: 10) {
-            CapsuleText(text: "First")
-            CapsuleText(text: "Second")
-        }
+        
     }
 }
 
