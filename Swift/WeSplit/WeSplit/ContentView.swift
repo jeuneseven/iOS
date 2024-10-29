98a106318c7294019f451975c76c69f91c8f770e
jeuneseven <jeuneseven@gmail.com>
2024-10-30 07:16:50 +1300

    Update ContentView.swift

--- Swift/WeSplit/WeSplit/ContentView.swift
+++ Swift/WeSplit/WeSplit/ContentView.swift
@@ -9,9 +9,7 @@ import SwiftUI
 //conform View protocol, must return some view
 struct ContentView: View {
     var body: some View {
-        NavigationStack {
-            
-        }
+        
     }
 }
 
