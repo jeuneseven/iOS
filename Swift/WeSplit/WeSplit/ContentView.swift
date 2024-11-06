29980fc32f09c67b9d16375931362879c668afdd
jeuneseven <jeuneseven@gmail.com>
2024-11-06 22:44:11 +1300

    Update ContentView.swift

--- Swift/WeSplit/WeSplit/ContentView.swift
+++ Swift/WeSplit/WeSplit/ContentView.swift
@@ -8,12 +8,6 @@
 import SwiftUI
 /**
  Core skills: Swift, SwiftUI, Working with data, Networking, Version Control
- Extension skills that are nice to have – the ones that will set you apart from the pack, if you have the time to learn them
- Common mistakes people make when they are trying to learn.
- Resources you can use to learn Swift that are completely free.
- How you can connect to the iOS development community.
- An idea of how long it will take to reach your goal.
- Preparing to apply for your first job
  */
 struct ContentView: View {
     var body: some View {
