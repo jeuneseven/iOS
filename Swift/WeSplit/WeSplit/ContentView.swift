7645ccaf6101819ebcee2497b4855a547a2e3778
jeuneseven <jeuneseven@gmail.com>
2024-11-25 17:57:41 +0800

    Update ContentView.swift

--- Swift/WeSplit/WeSplit/ContentView.swift
+++ Swift/WeSplit/WeSplit/ContentView.swift
@@ -8,15 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
     // will return something that conforms to the View protocol, which is our layout
     var body: some View {
-        VStack () {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello World!")
+        Button("tapCount \(tapCount)") {
+            tapCount += 1;
         }
-        .padding()
+        
+//        VStack () {
+//            Image(systemName: "globe")
+//                .imageScale(.large) // View modifier
+//                .foregroundStyle(.tint) // View modifier
+//            Text("Hello World!")
+//        }
+//        .padding() // View modifier
+//        if #available(iOS 16.0, *) {
+//            NavigationStack {
+//                Form() {
+//                    Section {
+//                        HStack () {
+//                            Image(systemName: "globe")
+//                                .imageScale(.large)
+//                                .foregroundStyle(.tint)
+//                            Text("Hello World!")
+//                        }
+//                        .padding()
+//                    }
+//                    
+//                    Section {
+//                        HStack () {
+//                            Image(systemName: "globe")
+//                                .imageScale(.large)
+//                                .foregroundStyle(.tint)
+//                            Text("Hello World!")
+//                        }
+//                        .padding()
+//                        
+//                        HStack () {
+//                            Image(systemName: "globe")
+//                                .imageScale(.large)
+//                                .foregroundStyle(.tint)
+//                            Text("Hello World!")
+//                        }
+//                        .padding()
+//                    }
+//                }
+//                .navigationTitle("SwiftUI")
+//                .navigationBarTitleDisplayMode(.inline)
+//            }
+//        } else {
+//            // Fallback on earlier versions
+//        }
     }
 }
 // This is a special piece of code that won’t actually form part of your final app that goes to the App Store, but is instead specifically for Xcode to use so it can show a preview of your UI design alongside your code.
