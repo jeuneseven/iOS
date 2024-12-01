5dd3c0129a583eb4f938d0d36dee6667307449f8
jeuneseven <jeuneseven@gmail.com>
2024-12-01 10:36:45 +0800

    Update ContentView.swift

--- Swift/WeSplit/WeSplit/ContentView.swift
+++ Swift/WeSplit/WeSplit/ContentView.swift
@@ -14,25 +14,66 @@ struct ContentView: View {
 //    let people = ["A", "B", "C"]
 //    @State private var selectedPeople = "A"
     
-    @State private var check = 0.0
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson:Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
     
     // will return something that conforms to the View protocol, which is our layout
     var body: some View {
-        Form {
-            if #available(iOS 16, *) {
-                TextField("Please input", value: $check, format:.currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    .keyboardType(.decimalPad)
-            } else {
-                TextField("Please input", value: $check, format:.currency(code:"USD"))
-                    .keyboardType(.decimalPad)
+        if #available(iOS 16.0, *) {
+            NavigationStack {
+                Form {
+                    Section {
+                        TextField("Please input", value: $checkAmount, format:.currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            .keyboardType(.decimalPad)
+                            .focused($amountIsFocused)
+                        Picker("Number of people", selection: $numberOfPeople) {
+                            ForEach (2..<10){
+                                Text("\($0) People")
+                            }
+                        }
+                        .pickerStyle(.navigationLink)
+                    }
+                    
+                    Section ("Select your tip") {
+                        Picker("TipPercentage", selection: $tipPercentage) {
+                            ForEach (tipPercentages, id:\.self) {
+                                Text($0, format:.percent)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    
+                    Section {
+                        Text(totalPerPerson, format:.currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    }
+                }
+                .navigationTitle("WeSplit")
+                .toolbar {
+                    if amountIsFocused {
+                        Button("Done") {
+                            amountIsFocused = false
+                        }
+                    }
+                }
             }
-            if #available(iOS 16, *) {
-                Text(check, format:.currency(code: Locale.current.currency?.identifier ?? "USD"))
-            } else {
-                TextField("Please input", value: $check, format:.currency(code:"USD"))
-                    .keyboardType(.decimalPad)
-            }
+        } else {
+            // Fallback on earlier versions
         }
+        
         
 //        Form () {
 //            Picker("Selected people", selection: $selectedPeople) {
