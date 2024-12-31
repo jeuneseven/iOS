d08be857533f2cfa6513c43a64677c3654d6893d
jeuneseven <jeuneseven@gmail.com>
2025-01-01 06:53:51 +0800

    Update Contents.swift

--- Swift/MyPlayground.playground/Pages/Page06.xcplaygroundpage/Contents.swift
+++ Swift/MyPlayground.playground/Pages/Page06.xcplaygroundpage/Contents.swift
@@ -1,10 +1,15 @@
 //Protocol
 protocol Vehicle {
+    var name: String { get }
+    var currentPassengers: Int { get set }
     func estimateTime(for distance: Int) -> Int
     func travel(distance: Int)
 }
 
 struct Car: Vehicle {
+    let name = "Car"
+    var currentPassengers = 1
+    
     func estimateTime(for distance: Int) -> Int {
         distance / 50
     }
@@ -18,7 +23,20 @@ struct Car: Vehicle {
     }
 }
 
-func commute(distance: Int, using vehicle: Car) {
+struct Bicycle: Vehicle {
+    let name = "Bicycle"
+    var currentPassengers = 1
+    
+    func estimateTime(for distance: Int) -> Int {
+        distance / 10
+    }
+
+    func travel(distance: Int) {
+        print("I'm cycling \(distance)km.")
+    }
+}
+
+func commute(distance: Int, using vehicle: Vehicle) {
     if vehicle.estimateTime(for: distance) > 100 {
         print("That's too slow! I'll try a different vehicle.")
     } else {
@@ -26,8 +44,20 @@ func commute(distance: Int, using vehicle: Car) {
     }
 }
 
+func getTravelEstimates(using vehicles: [Vehicle], distance: Int) {
+    for vehicle in vehicles {
+        let estimate = vehicle.estimateTime(for: distance)
+        print("\(vehicle.name): \(estimate) hours to travel \(distance)km")
+    }
+}
+
 let car = Car()
 commute(distance: 100, using: car)
+
+let bike = Bicycle()
+commute(distance: 50, using: bike)
+
+getTravelEstimates(using: [car, bike], distance: 150)
 
 //needs a type annotation
 protocol Swimmable {
