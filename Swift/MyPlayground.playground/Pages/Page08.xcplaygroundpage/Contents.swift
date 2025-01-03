910de5183ab2a8a4fdb79ca1162c20bcf7edf756
jeuneseven <jeuneseven@gmail.com>
2025-01-04 03:04:13 +0800

    update

--- Swift/MyPlayground.playground/Pages/Page08.xcplaygroundpage/Contents.swift
+++ Swift/MyPlayground.playground/Pages/Page08.xcplaygroundpage/Contents.swift
@@ -1,7 +1,4 @@
 //: Checkpoint 7:https://www.hackingwithswift.com/quick-start/beginners/checkpoint-7
-//: Checkpoint 8: https://www.hackingwithswift.com/quick-start/beginners/checkpoint-8
 
 import Foundation
 
-
-//: [Next](@next)
---
+++ Swift/MyPlayground.playground/Pages/Page09.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+//: Checkpoint 8: https://www.hackingwithswift.com/quick-start/beginners/checkpoint-8
+
+import Foundation
+
+var greeting = "Hello, playground"
