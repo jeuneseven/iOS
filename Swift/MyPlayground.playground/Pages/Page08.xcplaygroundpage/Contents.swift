a1bb1a25be2b0681c503890390083a7fdc731e4f
jeuneseven <jeuneseven@gmail.com>
2025-01-03 17:18:42 +0800

    Update Contents.swift

--- Swift/MyPlayground.playground/Pages/Page08.xcplaygroundpage/Contents.swift
+++ Swift/MyPlayground.playground/Pages/Page08.xcplaygroundpage/Contents.swift
@@ -1,4 +1,5 @@
 //: Checkpoint 7:https://www.hackingwithswift.com/quick-start/beginners/checkpoint-7
+//: Checkpoint 8: https://www.hackingwithswift.com/quick-start/beginners/checkpoint-8
 
 import Foundation
 
