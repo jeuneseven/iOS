29264c89123cea0da200eeb9ffeff42016c00cb8
jeuneseven <jeuneseven@gmail.com>
2025-01-19 21:26:05 +0800

    Update ContentView.swift

--- Swift/WeSplit/WeSplit/ContentView.swift
+++ Swift/WeSplit/WeSplit/ContentView.swift
@@ -8,51 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
-    @State private var name = ""
-    
-    let array = ["A", "B", "C"]
-    @State private var selectedItem = "A"
-    
     var body: some View {
-        if #available(iOS 16.0, *) {
-            NavigationStack {
-                Form {
-                    Section {
-                        Text("Hello, world!")
-                    }
-                    Group {
-                        Button("TapCount: \(tapCount)") {
-                            tapCount += 1
-                        }
-                    }
-                    Section {
-                        TextField("Enter your name", text: $name)
-                        Text("Your name is \(name)")
-                    }
-                    
-                    Section {
-                        Picker("Select one item", selection: $selectedItem) {
-                            // SwiftUI needs to be able to identify every view on the screen uniquely, the strings themselves are unique.
-                            ForEach(array, id: \.self) {
-                                Text($0)
-                            }
-                        }
-                    }
-                    
-                    Section {
-                        ForEach(0..<10) {
-                            Text("Row \($0)")
-                        }
-                    }
-                }
-                .navigationTitle("SwiftUI")
-                
-            }
-        } else {
-            // Fallback on earlier versions
-        }
+        Text("Hello, world!")
     }
+    
+//    @State private var tapCount = 0
+//    @State private var name = ""
+//    
+//    let array = ["A", "B", "C"]
+//    @State private var selectedItem = "A"
+//    
+//    var body: some View {
+//        if #available(iOS 16.0, *) {
+//            NavigationStack {
+//                Form {
+//                    Section {
+//                        Text("Hello, world!")
+//                    }
+//                    Group {
+//                        Button("TapCount: \(tapCount)") {
+//                            tapCount += 1
+//                        }
+//                    }
+//                    Section {
+//                        TextField("Enter your name", text: $name)
+//                        Text("Your name is \(name)")
+//                    }
+//                    
+//                    Section {
+//                        Picker("Select one item", selection: $selectedItem) {
+//                            // SwiftUI needs to be able to identify every view on the screen uniquely, the strings themselves are unique.
+//                            ForEach(array, id: \.self) {
+//                                Text($0)
+//                            }
+//                        }
+//                    }
+//                    
+//                    Section {
+//                        ForEach(0..<10) {
+//                            Text("Row \($0)")
+//                        }
+//                    }
+//                }
+//                .navigationTitle("SwiftUI")
+//            }
+//        } else {
+//            // Fallback on earlier versions
+//        }
+//    }
 }
 // This is a special piece of code that won’t actually form part of your final app that goes to the App Store, but is instead specifically for Xcode to use so it can show a preview of your UI design alongside your code.
 #Preview {
