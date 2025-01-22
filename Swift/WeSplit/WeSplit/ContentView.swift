22e55cac5346eadde294d653e199fcfcbd531302
jeuneseven <jeuneseven@gmail.com>
2025-01-22 18:32:23 +0800

    Update ContentView.swift

--- Swift/WeSplit/WeSplit/ContentView.swift
+++ Swift/WeSplit/WeSplit/ContentView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
+        if #available(iOS 16.0, *) {
+            NavigationStack {
+                Form {
+                    // everything inside your form
+                }
+            }
+        } else {
+            // Fallback on earlier versions
+        }
     }
 //    @State private var tapCount = 0
 //    @State private var name = ""
