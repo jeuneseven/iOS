3caebd7fa17a729e6240cd6ff81003f13dae2402
jeuneseven <jeuneseven@gmail.com>
2025-01-24 05:50:43 +0800

    Update ContentView.swift

--- Swift/WeSplit/WeSplit/ContentView.swift
+++ Swift/WeSplit/WeSplit/ContentView.swift
@@ -8,12 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    
     var body: some View {
         if #available(iOS 16.0, *) {
             NavigationStack {
                 Form {
+                    Section {
+                        TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                            .keyboardType(.decimalPad)
+                        
+                        Picker("Number of People", selection: $numberOfPeople) {
+                            ForEach(2..<10) {
+                                Text("\($0) people")
+                            }
+                        }
+                    }
                     
+                    Section("How much tip do you want to leave?") {
+                        Picker("Tip percentage", selection: $tipPercentage) {
+                            ForEach(tipPercentages, id: \.self) {
+                                Text($0, format: .percent)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    
+                    Section {
+                        Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    }
                 }
+                .navigationTitle("WeSplit")
             }
         } else {
             // Fallback on earlier versions
