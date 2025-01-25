efc2ce9594ad7da6d62a6113bbcd08720c5dcde2
jeuneseven <jeuneseven@gmail.com>
2025-01-26 05:37:55 +0800

    Update ContentView.swift

--- Swift/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Swift/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,18 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack (spacing: 0) {
-                Color.blue
-                Color.red
-            }
-            
-            Text("Some Text")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-        }
-        .ignoresSafeArea()
+        
     }
 }
 
