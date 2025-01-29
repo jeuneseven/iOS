fa31341fd0003481c05dfd9b07ac1de3689b6931
jeuneseven <jeuneseven@gmail.com>
2025-01-29 20:41:18 +0800

    Update ContentView.swift

--- Swift/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Swift/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
+        var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+        var correctAnswer = Int.random(in: 0...2)
     }
 }
 
