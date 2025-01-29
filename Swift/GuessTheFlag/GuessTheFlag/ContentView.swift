ed13a85a3022023323e33ae946932e75220e96e2
jeuneseven <jeuneseven@gmail.com>
2025-01-30 07:27:33 +0800

    Update ContentView.swift

--- Swift/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Swift/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,8 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
-        var correctAnswer = Int.random(in: 0...2)
+//        var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+//        var correctAnswer = Int.random(in: 0...2)
+        
+//        if #available(iOS 16.0, *) {
+//            Text("Your content")
+//                .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                .foregroundStyle(.white)
+//                .background(.red.gradient)
+//        } else {
+//            // Fallback on earlier versions
+//        }
+        
+//        AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
+        
+//        RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
+        
+//        LinearGradient(stops: [
+//            Gradient.Stop(color: .white, location: 0.45),
+//            Gradient.Stop(color: .black, location: 0.55),
+//        ], startPoint: .top, endPoint: .bottom)
+        
+//        LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
+        
+//        // use shift+command+a to toggle appearance
+//        ZStack {
+//            VStack(spacing: 0) {
+//                Color.red
+//                Color.blue
+//            }
+//
+//            Text("Your content")
+//                .foregroundStyle(.secondary)
+//                .padding(50)
+//                .background(.ultraThinMaterial)
+//        }
+//        .ignoresSafeArea()
     }
 }
 
