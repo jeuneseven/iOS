7816b9c06194332df348e1135d5f069d1f8f207a
jeuneseven <jeuneseven@gmail.com>
2025-02-08 05:50:55 +0800

    Update ContentView.swift

--- Swift/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Swift/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,32 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-//    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
-//    var correctAnswer = Int.random(in: 0...2)
-//    
-//    var body: some View {
-//        ZStack {
-//            Color.blue
-//                .ignoresSafeArea()
-//            VStack(spacing: 30) {
-//                VStack {
-//                    Text("Tap the flag of")
-//                        .foregroundStyle(.white)
-//
-//                    Text(countries[correctAnswer])
-//                        .foregroundStyle(.white)
-//                }
-//                
-//                ForEach(0..<3) { number in
-//                    Button {
-//                        // flag was tapped
-//                    } label: {
-//                        Image(countries[number])
-//                    }
-//                }
-//            }
-//        }
-//    }
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
+    var body: some View {
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        // flag was tapped
+                    } label: {
+                        Image(countries[number])
+                    }
+                }
+            }
+        }
+    }
     
 //    @State private var showingAlert = false
 //    var body: some View {
