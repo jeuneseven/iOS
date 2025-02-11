aa19e785d7f586f94808ea72a8835498de5d2258
jeuneseven <jeuneseven@gmail.com>
2025-02-12 05:05:17 +0800

    Update ContentView.swift

--- Swift/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Swift/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,82 +8,96 @@
 import SwiftUI
 
 struct ContentView: View {
-//    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
-//    var correctAnswer = Int.random(in: 0...2)
-//    
-//    var body: some View {
-//        ZStack {
-//            Color.blue
-//                .ignoresSafeArea()
-//            VStack(spacing: 30) {
-//                VStack {
-//                    Text("Tap the flag of")
-//                        .foregroundStyle(.white)
-//
-//                    Text(countries[correctAnswer])
-//                        .foregroundStyle(.white)
-//                }
-//                
-//                ForEach(0..<3) { number in
-//                    Button {
-//                        // flag was tapped
-//                    } label: {
-//                        Image(countries[number])
-//                    }
-//                }
-//            }
-//        }
-//    }
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+
+    var correctAnswer = Int.random(in: 0...2)
     
-    @State private var showingAlert = false
     var body: some View {
-        Button("Show Alert") {
-            showingAlert = true
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        // flag was tapped
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                    }
+                }
+            }
         }
-        .alert("Important message", isPresented: $showingAlert) {
-            Button("Delete", role: .destructive) { }
-            Button("Cancel", role: .cancel) { }
-        } message: {
-            Text("Please read this.")
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
         }
+
+        showingScore = true
+    }
+    
+//    @State private var showingAlert = false
+//    var body: some View {
+//        Button("Show Alert") {
+//            showingAlert = true
+//        }
+//        .alert("Important message", isPresented: $showingAlert) {
+//            Button("Delete", role: .destructive) { }
+//            Button("Cancel", role: .cancel) { }
+//        } message: {
+//            Text("Please read this.")
+//        }
         
-        VStack(spacing:10) {
-            Button {
-                print("Edit button was tapped")
-            } label: {
-                Label("Edit", systemImage: "pencil")
-                    .padding()
-                    .foregroundStyle(.white)
-                    .background(.red)
-            }
-            
-            Button("Edit", systemImage: "pencil", action: printSomething)
-            
-            Button {
-                print("Edit button was tapped")
-            } label: {
-                Image(systemName: "pencil")
-            }
-            
-            Button {
-                print("Button was tapped")
-            } label: {
-                Text("Tap me!")
-                    .padding()
-                    .foregroundStyle(.white)
-                    .background(.red)
-            }
-            
-            Button("Button 1") { }
-                .buttonStyle(.bordered)
-            Button("Button 2", role: .destructive) { }
-                .buttonStyle(.bordered)
-            Button("Button 3") { }
-                .buttonStyle(.borderedProminent)
-                .tint(.mint)
-            Button("Button 4", role: .destructive) { }
-                .buttonStyle(.borderedProminent)
-        }
+//        VStack(spacing:10) {
+//            Button {
+//                print("Edit button was tapped")
+//            } label: {
+//                Label("Edit", systemImage: "pencil")
+//                    .padding()
+//                    .foregroundStyle(.white)
+//                    .background(.red)
+//            }
+//            
+//            Button("Edit", systemImage: "pencil", action: printSomething)
+//            
+//            Button {
+//                print("Edit button was tapped")
+//            } label: {
+//                Image(systemName: "pencil")
+//            }
+//            
+//            Button {
+//                print("Button was tapped")
+//            } label: {
+//                Text("Tap me!")
+//                    .padding()
+//                    .foregroundStyle(.white)
+//                    .background(.red)
+//            }
+//            
+//            Button("Button 1") { }
+//                .buttonStyle(.bordered)
+//            Button("Button 2", role: .destructive) { }
+//                .buttonStyle(.bordered)
+//            Button("Button 3") { }
+//                .buttonStyle(.borderedProminent)
+//                .tint(.mint)
+//            Button("Button 4", role: .destructive) { }
+//                .buttonStyle(.borderedProminent)
+//        }
         
 //        if #available(iOS 16.0, *) {
 //            Text("Your content")
@@ -120,10 +134,10 @@ struct ContentView: View {
 //        .ignoresSafeArea()
     }
     
-    func printSomething() {
-        print("Edit button was tapped")
-    }
-}
+//    func printSomething() {
+//        print("Edit button was tapped")
+//    }
+//}
 
 #Preview {
     ContentView()
