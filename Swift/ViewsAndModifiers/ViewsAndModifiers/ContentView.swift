66b4eb517876de77f12bc76c710fc559fefbdc3b
jeuneseven <jeuneseven@gmail.com>
2025-02-16 07:13:00 +0800

    Update ContentView.swift

--- Swift/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Swift/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -9,14 +9,41 @@ import SwiftUI
 
 struct ContentView: View {    
     var body: some View {
-        VStack {
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundStyle(.tint)
-                    Text("Hello, world!")
-                }
-                .padding()
-                .background(.red)
+        Text("Hello, world!")
+            .padding()
+            .background(.red)
+            .padding()
+            .background(.blue)
+            .padding()
+            .background(.green)
+            .padding()
+            .background(.yellow)
+        
+//        Button("Hello, world!") {
+//            // AnyView
+//            print(type(of: self.body))
+//        }
+//        // view modifier's order matters
+//        .frame(width: 200, height: 200)
+//        .background(.red)
+        
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//        }
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        .background(.red)
+//        // there is an UIHostingController behind the view, it's belong to UIKit
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//        .background(.red)
     }
 }
 
