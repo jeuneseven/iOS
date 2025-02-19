3feed985d2199f2bcc9a828a4074edcdef0e7f99
jeuneseven <jeuneseven@gmail.com>
2025-02-20 04:12:04 +0800

    Update ContentView.swift

--- Swift/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Swift/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,40 +7,126 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var useRedText = false
-    
-    var motto1: some View {
-        Text("Draco dormiens")
-    }
-    
-    let motto2 = Text("nunquam titillandus")
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int, Int) -> Content
 
-    var spells: some View {
-        Group {
-            Text("Lumos")
-            Text("Obliviate")
-        }
-    }
-    
-    @ViewBuilder var anotherSpells: some View {
-        Text("Lumos")
-        Text("Obliviate")
-    }
-    
     var body: some View {
         VStack {
-            motto1
-                .foregroundStyle(.red)
-            motto2
-                .foregroundStyle(.blue)
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
         }
-        HStack {
-            spells
+    }
+}
+
+//struct Watermark: ViewModifier {
+//    var text: String
+//
+//    func body(content: Content) -> some View {
+//        ZStack(alignment: .bottomTrailing) {
+//            content
+//            Text(text)
+//                .font(.caption)
+//                .foregroundStyle(.white)
+//                .padding(5)
+//                .background(.black)
+//        }
+//    }
+//}
+//
+//extension View {
+//    func watermarked(with text: String) -> some View {
+//        modifier(Watermark(text: text))
+//    }
+//}
+
+//struct Title: ViewModifier {
+//    func body(content: Content) -> some View {
+//        content
+//            .font(.largeTitle)
+//            .foregroundStyle(.white)
+//            .padding()
+//            .background(.blue)
+//            .clipShape(.rect(cornerRadius: 10))
+//    }
+//}
+//
+//extension View {
+//    func titleStyle() -> some View {
+//        modifier(Title())
+//    }
+//}
+
+//struct CapsuleText: View {
+//    var text: String
+//
+//    var body: some View {
+//        Text(text)
+//            .font(.largeTitle)
+//            .padding()
+//            .foregroundStyle(.white)
+//            .background(.blue)
+//            .clipShape(.capsule)
+//    }
+//}
+
+struct ContentView: View {
+//    @State private var useRedText = false
+//    
+//    var motto1: some View {
+//        Text("Draco dormiens")
+//    }
+//    
+//    let motto2 = Text("nunquam titillandus")
+//
+//    var spells: some View {
+//        Group {
+//            Text("Lumos")
+//            Text("Obliviate")
+//        }
+//    }
+//    
+//    @ViewBuilder var anotherSpells: some View {
+//        Text("Lumos")
+//        Text("Obliviate")
+//    }
+    
+    var body: some View {
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
         }
-        ZStack {
-            anotherSpells
-        }
+        
+//        Color.blue
+//            .frame(width: 300, height: 200)
+//            .watermarked(with: "Hacking with Swift")
+        
+//        Text("Hello World")
+//            .titleStyle()
+        
+//        VStack(spacing: 10) {
+//            CapsuleText(text: "First")
+//            CapsuleText(text: "Second")
+//        }
+        
+//        VStack {
+//            motto1
+//                .foregroundStyle(.red)
+//            motto2
+//                .foregroundStyle(.blue)
+//        }
+//        HStack {
+//            spells
+//        }
+//        ZStack {
+//            anotherSpells
+//        }
         
 //        VStack {
 //            Text("Gryffindor")
