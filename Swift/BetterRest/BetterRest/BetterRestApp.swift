3563c186f330f11810b7bb935f15cd9ec6309882
jeuneseven <jeuneseven@gmail.com>
2025-02-21 04:14:00 +0800

    BetterRest init

---
+++ Swift/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by seven on 2025/2/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
