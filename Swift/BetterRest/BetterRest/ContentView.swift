32a6c15288f0c9daaa6c8a933033470967ec3fa7
jeuneseven <jeuneseven@gmail.com>
2025-02-22 17:42:58 +0800

    Update ContentView.swift

--- Swift/BetterRest/BetterRest/ContentView.swift
+++ Swift/BetterRest/BetterRest/ContentView.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
 
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        
     }
+    
+//    @State private var sleepAmount = 8.0
+//
+//    var body: some View {
+//        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+//    }
 }
 
 #Preview {
