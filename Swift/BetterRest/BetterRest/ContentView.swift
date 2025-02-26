98a2d7cb719d2c9a1981cdabef3d5eb01645f0f7
jeuneseven <jeuneseven@gmail.com>
2025-02-27 03:32:11 +0800

    Update ContentView.swift

--- Swift/BetterRest/BetterRest/ContentView.swift
+++ Swift/BetterRest/BetterRest/ContentView.swift
@@ -11,6 +11,13 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
 
     var body: some View {
+        DatePicker("Please enter a date", selection: $wakeUp)
+        
+        DatePicker("", selection: $wakeUp)
+
+        DatePicker("Please enter a date", selection: $wakeUp)
+            .labelsHidden()
+        
         DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
         
         DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
