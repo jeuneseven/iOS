3786166b6d768164673e9d42dda557f69fa19599
jeuneseven <jeuneseven@gmail.com>
2025-02-26 18:30:10 +0800

    Update ContentView.swift

--- Swift/BetterRest/BetterRest/ContentView.swift
+++ Swift/BetterRest/BetterRest/ContentView.swift
@@ -16,13 +16,13 @@ struct ContentView: View {
         DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
     }
     
-    func exampleDates() {
-        // create a second Date instance set to one day in seconds from now
-        let tomorrow = Date.now.addingTimeInterval(86400)
-
-        // create a range from those two
-        let range = Date.now...tomorrow
-    }
+//    func exampleDates() {
+//        // create a second Date instance set to one day in seconds from now
+//        let tomorrow = Date.now.addingTimeInterval(86400)
+//
+//        // create a range from those two
+//        let range = Date.now...tomorrow
+//    }
     
 //    @State private var sleepAmount = 8.0
 //
