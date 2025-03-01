1cb010959602e7ba8a3030efbe5a62b999d08c04
jeuneseven <jeuneseven@gmail.com>
2025-03-02 02:40:31 +0800

    BestRestML init

--- Swift/BetterRest/BetterRest/ContentView.swift
+++ Swift/BetterRest/BetterRest/ContentView.swift
@@ -10,8 +10,12 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        Text(Date.now.formatted(date: .long, time: .omitted))
+        Text("Hello World").padding()
     }
+    
+//    var body: some View {
+//        Text(Date.now.formatted(date: .long, time: .omitted))
+//    }
     
 //    func trivialExample() {
 //        var components = DateComponents()
