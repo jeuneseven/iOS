c8e8a4a4d9f934fd548bba4e02ea07205f75b769
jeuneseven <jeuneseven@gmail.com>
2025-03-07 14:35:15 +0800

    WordScramble init

---
+++ Swift/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by seven on 2025/3/6.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
