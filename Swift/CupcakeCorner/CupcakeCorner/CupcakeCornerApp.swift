b688fefde22aa1147e8d08ee00bec8e16abc1a26
jeuneseven <jeuneseven@gmail.com>
2025-07-21 06:44:14 +0800

    update

---
+++ Swift/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by seven on 2025/7/19.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
