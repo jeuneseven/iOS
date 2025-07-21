114a623af368a2837505bf7c96f27513cbb54c41
jeuneseven <jeuneseven@gmail.com>
2025-07-22 06:30:32 +0800

    Update ContentView.swift

--- Swift/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ Swift/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -17,19 +17,77 @@ struct Result: Codable {
     var collectionName: String
 }
 
+@Observable
+class User: Codable {
+//    enum CodingKeys: String, CodingKey {
+//            case _name = "name"
+//    }
+    
+    var name = "Taylor"
+}
+
 struct ContentView: View {
-    @State private var results = [Result]()
+    @State private var counter = 0
+    
+//    @State private var results = [Result]()
+//    @State private var username = ""
+//    @State private var email = ""
+    
+//    var disableForm: Bool {
+//        username.count < 5 || email.count < 5
+//    }
+//    
+//    func encodeTaylor() {
+//        let data = try! JSONEncoder().encode(User())
+//        let str = String(decoding: data, as: UTF8.self)
+//        print(str)
+//    }
     
     var body: some View {
-//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 3)
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
-            image
-                .resizable()
-                .scaledToFit()
-        } placeholder: {
-            Color.red
+        Button("Tap count: \(counter)") {
+            counter += 1
         }
-        .frame(width: 200, height: 200)
+        .sensoryFeedback(.increase, trigger: counter)
+        
+//        Button("Encode Taylor", action: encodeTaylor)
+//        Form {
+//            Section {
+//                TextField("Username", text: $username)
+//                TextField("Email", text: $email)
+//            }
+//
+//            Section {
+//                Button("Create account") {
+//                    print("Creating account…")
+//                }
+//            }
+//            .disabled(disableForm)
+////            .disabled(username.isEmpty || email.isEmpty)
+//        }
+        
+//        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
+//            if let image = phase.image {
+//                image
+//                    .resizable()
+//                    .scaledToFit()
+//            } else if phase.error != nil {
+//                Text("There was an error loading the image.")
+//            } else {
+//                ProgressView()
+//            }
+//        }
+//        .frame(width: 200, height: 200)
+        
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 3)
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+//            image
+//                .resizable()
+//                .scaledToFit()
+//        } placeholder: {
+////            Color.red
+//            ProgressView()
+//        }
+//        .frame(width: 200, height: 200)
         
 //        List(results, id: \.trackId) { item in
 //            VStack(alignment: .leading) {
@@ -43,22 +101,22 @@ struct ContentView: View {
 //        }
     }
     
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        }
-    }
+//    func loadData() async {
+//        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+//            print("Invalid URL")
+//            return
+//        }
+//        
+//        do {
+//            let (data, _) = try await URLSession.shared.data(from: url)
+//
+//            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+//                results = decodedResponse.results
+//            }
+//        } catch {
+//            print("Invalid data")
+//        }
+//    }
 }
 
 #Preview {
