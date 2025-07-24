d3adc089d5eba208ecbb4be576f0d0090c8821fb
jeuneseven <jeuneseven@gmail.com>
2025-07-25 07:37:33 +0800

    Update ContentView.swift

--- Swift/TodoList/TodoList/ContentView.swift
+++ Swift/TodoList/TodoList/ContentView.swift
@@ -7,48 +7,12 @@
 
 import SwiftUI
 
-struct TableViewWrapper: UIViewControllerRepresentable {
-    class Coordinator: NSObject, UITableViewDataSource, UITableViewDelegate {
-        var parent: TableViewWrapper
-
-        init(parent: TableViewWrapper) {
-            self.parent = parent
-        }
-
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return parent.items.count
-        }
-
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-            cell.textLabel?.text = parent.items[indexPath.row]
-            return cell
-        }
-    }
-
-    var items: [String]
-
-    func makeCoordinator() -> Coordinator {
-        return Coordinator(parent: self)
-    }
-
-    func makeUIViewController(context: Context) -> UITableViewController {
-        let tableViewController = UITableViewController()
-        tableViewController.tableView.dataSource = context.coordinator
-        tableViewController.tableView.delegate = context.coordinator
-        tableViewController.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        return tableViewController
-    }
-
-    func updateUIViewController(_ uiViewController: UITableViewController, context: Context) {
-        uiViewController.tableView.reloadData()
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        TableViewWrapper(items: ["Item 1", "Item 2", "Item 3", "Item 4"])
-            .navigationBarTitle("UITableView Example", displayMode: .inline)
+        List {
+
+        }
+        .ignoresSafeArea()
     }
 }
 
