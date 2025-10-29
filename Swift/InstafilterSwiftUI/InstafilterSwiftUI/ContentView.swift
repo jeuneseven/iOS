8fce504118432e28b6ad4bf91b5081e09644fd07
jeuneseven <jeuneseven@gmail.com>
2025-10-30 05:15:03 +0800

    update

---
+++ Swift/InstafilterSwiftUI/InstafilterSwiftUI/ContentView.swift
@@ -0,0 +1,119 @@
+//
+//  ContentView.swift
+//  InstafilterSwiftUI
+//
+//  Created by seven on 2025/8/21.
+//
+
+import SwiftUI
+import PhotosUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import StoreKit
+
+struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var showingFilters = false
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
+    @AppStorage("filter count") var filterCount = 0
+    @Environment(\.requestReview) var requestReview
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
+                }
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
+                }
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    if let processedImage {
+                        ShareLink(item: processedImage, preview: SharePreview("Instafilter image", image: processedImage))
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+            .confirmationDialog("Select a filter", isPresented: $showingFilters) {
+                Button("Crystallize") {setFilter(CIFilter.crystallize())}
+                Button("Edges") {setFilter(CIFilter.edges())}
+                // ...
+                Button("Cancel", role: .cancel) {}
+            }
+        }
+    }
+    
+    func changeFilter() {
+        showingFilters = true
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else {return}
+            guard let inputImage = UIImage(data: imageData) else {return}
+            
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey:kCIInputImageKey)
+            applyProcessing()
+        }
+    }
+    
+    func applyProcessing() {
+//        currentFilter.intensity = Float(filterIntensity)
+//        currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        }
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(filterIntensity, forKey: kCIInputRadiusKey)
+        }
+        // ...
+        guard let outputImage = currentFilter.outputImage else {return}
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {return}
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
+    }
+    
+    @MainActor func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
+        
+        filterCount += 1
+        
+        if filterCount >= 10 {
+            requestReview()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift/InstafilterSwiftUI/InstafilterSwiftUI/InstafilterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterSwiftUIApp.swift
+//  InstafilterSwiftUI
+//
+//  Created by seven on 2025/8/21.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
