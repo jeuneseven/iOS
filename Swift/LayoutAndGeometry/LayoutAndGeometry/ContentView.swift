bda0a1bd11caba2d268791480a08ddcefe9be95d
jeuneseven <jeuneseven@gmail.com>
2025-11-19 04:46:02 +0800

    update

---
+++ Swift/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by seven on 2025/8/24.
+//
+
+import SwiftUI
+
+
+
+
+
+struct ContentView: View {
+
+    
+    var body: some View {
+                
+
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift/LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by seven on 2025/8/24.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
