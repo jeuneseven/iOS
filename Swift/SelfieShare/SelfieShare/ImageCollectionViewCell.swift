64df00ecf672c04b6f2f02725c04e1a85c852cda
jeuneseven <jeuneseven@gmail.com>
2025-11-23 05:35:10 +0800

    update

---
+++ Swift/SelfieShare/SelfieShare/ImageCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImageCollectionViewCell.swift
+//  SelfieShare
+//
+//  Created by seven on 2025/8/11.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+}
