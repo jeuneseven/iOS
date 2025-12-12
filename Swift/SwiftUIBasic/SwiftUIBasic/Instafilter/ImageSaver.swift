48d7ed44a5528819009a4a820df6e27e78ffe41f
jeuneseven <jeuneseven@gmail.com>
2025-12-13 05:46:15 +0800

    Create ImageSaver.swift

---
+++ Swift/SwiftUIBasic/SwiftUIBasic/Instafilter/ImageSaver.swift
@@ -0,0 +1,25 @@
+//
+//  ImageSaver.swift
+//  SwiftUIBasic
+//
+//  Created by seven on 2025/9/17.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler:(() -> Void)?
+    var errorHandler:((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
