fda318fe5f48d84ddaf8a4147fc912229ca1623d
jeuneseven <jeuneseven@gmail.com>
2025-12-14 13:02:12 +0800

    create sample data

---
+++ Swift/TradeMeDemo/TradeMeDemo/TradeMeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeMeDemoApp.swift
+//  TradeMeDemo
+//
+//  Created by seven on 2025/12/11.
+//
+
+import SwiftUI
+
+@main
+struct TradeMeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
