2198a3cc536894e81a443a5ccfba8946902d5612
jeuneseven <jeuneseven@gmail.com>
2025-12-19 06:05:10 +0800

    Create ResortDetailView.swift

---
+++ Swift/SnowSeeker/SnowSeeker/Views/ResortDetailView.swift
@@ -0,0 +1,49 @@
+//
+//  ResortDetailView.swift
+//  SnowSeeker
+//
+//  Created by seven on 2025/9/7.
+//
+
+import SwiftUI
+
+struct ResortDetailView: View {
+    let resort: Resort
+    
+    var size: String {
+        switch resort.size {
+        case 1: "Small"
+        case 2: "Average"
+        default: "Large"
+        }
+    }
+    
+    var price: String {
+        String(repeating: "$", count: resort.price)
+    }
+    
+    var body: some View {
+        Group {
+            VStack {
+                Text("Size")
+                    .font(.caption.bold())
+                
+                Text(size)
+                    .font(.title3)
+            }
+            
+            VStack {
+                Text("Price")
+                    .font(.caption.bold())
+                
+                Text(price)
+                    .font(.title3)
+            }
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
+
+#Preview {
+    ResortDetailView(resort: .example)
+}
