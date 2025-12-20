a200542eb4e7d90514e6ef81258e4d197f1f52af
jeuneseven <jeuneseven@gmail.com>
2025-12-20 09:31:40 +0800

    Create ContentView.swift

---
+++ Swift/SnowSeeker/SnowSeeker/Views/ContentView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by seven on 2025/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    @State private var favorites = Favorites()
+    @State private var searchText = ""
+    var filteredResorts: [Resort] {
+        if searchText.isEmpty {
+            resorts
+        } else {
+            resorts.filter { resort in
+                resort.name.localizedStandardContains(searchText)
+            }
+        }
+    }
+    
+    var body: some View {
+        NavigationSplitView {
+            List(filteredResorts) { resort in
+                NavigationLink(value: resort) {
+                    HStack {
+                        Image(resort.country)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 25)
+                            .clipShape(.rect(cornerRadius: 5))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(.black, lineWidth: 1)
+                            )
+                        VStack(alignment: .leading) {
+                            Text(resort.name)
+                                .font(.headline)
+                            
+                            Text("\(resort.runs) runs")
+                                .foregroundStyle(.secondary)
+                        }
+                        
+                        if favorites.contains(resort) {
+                            Spacer()
+                            
+                            Image(systemName: "heart.fill")
+                                .accessibilityLabel("This is a favorite resort")
+                                .foregroundStyle(.red)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Resorts")
+            .searchable(text: $searchText)
+            .navigationDestination(for: Resort.self) { resort in
+                ResortView(resort: resort)
+            }
+        } detail: {
+            WelcomeView()
+        }
+        .environment(favorites)
+    }
+}
+
+#Preview {
+    ContentView()
+}
