d3370424c8add06eecd73573a4602ea4870a1d2b
jeuneseven <jeuneseven@gmail.com>
2025-12-27 09:26:14 +1300

    CodeBreaker init

---
+++ Swift/CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by seven on 2025/12/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
