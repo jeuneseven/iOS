b999008d9baf8c6a5043f1b3c43cf109fafe028c
jeuneseven <jeuneseven@gmail.com>
2026-01-07 09:33:40 +1300

    Create ViewModel.swift

---
+++ Swift/Memorize/Memorize/ViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  ViewModel.swift
+//  Memorize
+//
+//  Created by seven on 2026/1/5.
+//
+
+import SwiftUI
+import Foundation
+import Observation
+
+@Observable
+class ViewModel {
+    private static let emojis = ["😈", "👻", "🎃", "🤡", "❤️", "🍚", "🏀", "🍺", "🐷", "🐸", "🐮", "🐱"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 12) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "⁇"
+            }
+        }
+    }
+    
+    private var game: MemoryGame<String> = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return game.cards
+    }
+    
+    func shuffle() {
+        game.shuffle()
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        game.choose(card)
+    }
+}
