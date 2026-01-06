86701c52c95f27bb87576418cd017337916cfc53
jeuneseven <jeuneseven@gmail.com>
2026-01-07 09:33:35 +1300

    Create MemoryGame.swift

---
+++ Swift/Memorize/Memorize/MemoryGame.swift
@@ -0,0 +1,68 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by seven on 2026/1/5.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> where CardContent: Equatable {
+    struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
+        var id: String
+        
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+        
+        var debugDescription: String {
+            "\(id): \(content) \(isFaceUp ? "up" : "down") \(isMatched ? "matched" : "")"
+        }
+    }
+    
+    private(set) var cards: Array<Card>
+    
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter { cards[$0].isFaceUp }.only }
+        set { cards.indices.forEach { cards[$0].isFaceUp = (newValue == $0) } }
+    }
+    
+    mutating func choose(_ card: Card) {
+        print("chose \(card)")
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
+                if let protentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                    if cards[chosenIndex].content == cards[protentialMatchIndex].content {
+                        cards[chosenIndex].isMatched = true
+                        cards[protentialMatchIndex].isMatched = true
+                    }
+                } else {
+                    indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+                }
+                
+                cards[chosenIndex].isFaceUp = true
+            }
+            
+        }
+    }
+    
+    mutating func shuffle() {
+        cards.shuffle()
+    }
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = []
+        
+        for pairIndex in 0..<max(2, numberOfPairsOfCards) {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(id: "\(pairIndex + 1)a", content: content))
+            cards.append(Card(id: "\(pairIndex + 1)b", content: content))
+        }
+    }
+}
+
+extension Array {
+    var only: Element? {
+        count == 1 ? first : nil
+    }
+}
