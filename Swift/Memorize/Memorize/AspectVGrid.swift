dcf49b61048ff1a8f4a36891411e930413fd3ce0
jeuneseven <jeuneseven@gmail.com>
2026-01-09 11:57:55 +1300

    Create AspectVGrid.swift

---
+++ Swift/Memorize/Memorize/AspectVGrid.swift
@@ -0,0 +1,53 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by seven on 2026/1/8.
+//
+
+import SwiftUI
+
+struct AspectVGrid<Item: Identifiable, ItemView: View>: View {
+    var items: [Item]
+    var aspectRatio: CGFloat = 1
+    var content: (Item) -> ItemView
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let gridItemSize = gridItemWidthThatFits(
+                count: items.count,
+                size: geometry.size,
+                atAspectRatio: aspectRatio)
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
+                ForEach(items) { item in
+                    
+                }
+            }
+        }
+    }
+    
+    private func gridItemWidthThatFits (
+        count: Int,
+        size: CGSize,
+        atAspectRatio aspectRatio: CGFloat
+    ) -> CGFloat {
+        let count = CGFloat(count)
+        var columnCount = 1.0
+        repeat {
+            let width = size.width / columnCount
+            let height = width / aspectRatio
+            
+            let rowCount = (count / columnCount).rounded(.up)
+            if rowCount * height < size.height {
+                return (size.width / columnCount).rounded(.down)
+            }
+            columnCount += 1
+        } while columnCount < count
+        
+        return min(size.width / count, size.height * aspectRatio).rounded(.down)
+    }
+}
+
+//#Preview {
+//    AspectVGrid()
+//}
