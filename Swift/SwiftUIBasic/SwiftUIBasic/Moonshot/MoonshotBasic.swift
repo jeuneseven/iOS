f0959472ecb873016d6fa0dcf7182965e6a3f23b
jeuneseven <jeuneseven@gmail.com>
2026-01-20 18:37:04 +1300

    Update MoonshotBasic.swift

--- Swift/SwiftUIBasic/SwiftUIBasic/Moonshot/MoonshotBasic.swift
+++ Swift/SwiftUIBasic/SwiftUIBasic/Moonshot/MoonshotBasic.swift
@@ -7,6 +7,35 @@
 
 import SwiftUI
 
+extension DateFormatter {
+    static let dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateFormat = "y-MM-dd"
+        return df
+    }()
+}
+
+extension Bundle {
+    // <T> is generics, <> also called Pulp Fiction brackets, T is stand for some type
+    func decoder<T: Codable>(_ file: String) -> T {
+        guard let loadFile = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to load file \(file)")
+        }
+        
+        guard let data = try? Data(contentsOf: loadFile) else {
+            fatalError("Failed to load data in file \(file)")
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(DateFormatter.dateFormatter)
+        do {
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+}
+
 //struct MoonshotBasic: View {
 //    let layout = [
 //        GridItem(.fixed(80)), // 80 is width
